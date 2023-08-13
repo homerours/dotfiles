9491744deaa98552d71e43484d2ca95acd514218
homerours <homerours@gmail.com>
2023-08-14 00:21:03 +0100

    git

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gl", ":Git log<CR>")
+vim.keymap.set("n", "<leader>gl", ":Gclog<CR>")
+vim.keymap.set("n", "<leader>gi", ":Gvdiffsplit<CR>")
