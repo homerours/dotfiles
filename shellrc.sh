898d0a57fea538c85001f694011cac92186986cb
homerours <homerours@gmail.com>
2026-02-08 15:13:10 +0000

    unif

--- shellrc.sh
+++ shellrc.sh
@@ -1,3 +1,22 @@
+# Use Homebrew git (2x faster than Apple's)
+export PATH="/usr/local/bin:$PATH"
+
+# Initialize gitstatus for super fast git prompt
+if [[ -n "$ZSH_VERSION" ]]; then
+    # Zsh
+    if [[ -f /usr/local/opt/gitstatus/gitstatus.plugin.zsh ]]; then
+        source /usr/local/opt/gitstatus/gitstatus.plugin.zsh
+        gitstatus_start MY
+    fi
+elif [[ -n "$BASH_VERSION" ]]; then
+    # Bash
+    if [[ -f /usr/local/opt/gitstatus/gitstatus.plugin.sh ]]; then
+        source /usr/local/opt/gitstatus/gitstatus.plugin.sh
+        gitstatus_stop 2>/dev/null
+        gitstatus_start -s -1 -u -1 -c -1 -d -1
+    fi
+fi
+
 export CLICOLOR=1
 export LSCOLORS="hx"
 
