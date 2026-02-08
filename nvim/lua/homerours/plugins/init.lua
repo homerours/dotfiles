5253e850e940742d82067548b6823cde90242eec
homerours <homerours@gmail.com>
2026-02-08 15:28:05 +0000

    clean

--- nvim/lua/homerours/plugins/init.lua
+++ nvim/lua/homerours/plugins/init.lua
@@ -9,11 +9,9 @@ return {
         "ibhagwan/fzf-lua",
     },
     {
-        -- dir = "/Users/leo/Documents/dev/jumper.nvim",
         "homerours/jumper.nvim",
-        dependencies = { 'nvim-telescope/telescope.nvim', "ibhagwan/fzf-lua" },
+        dependencies = { "ibhagwan/fzf-lua" },
         config = function()
-            -- local jumper = require("telescope").extensions.jumper
             local jumper = require("jumper.fzf-lua")
             vim.keymap.set('n', '<c-y>', function() jumper.jump_to_directory({ on_enter = 'find_files' }) end)
             vim.keymap.set('n', '<c-u>', jumper.jump_to_file)
@@ -22,8 +20,6 @@ return {
             require("jumper").setup({
                 jumper_max_results = 300,
                 jumper_max_completion_results = 3,
-                -- jumper_files = '/path/to/a/custom/file/database',
-                -- jumper_directories = '/Users/leo/datatest',
             })
         end,
     },
